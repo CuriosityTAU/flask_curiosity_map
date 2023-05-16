beee0e442254bc7d43c0cd32621ba51cb957f6fe
curiosity <curiosity@gorengordon.com>
2023-05-16 17:26:31 +0300

    first commit

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
 
+@app.route('/')
+def home():
+    return "Hello, World!"
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443,debug=True)
+    app.run(host='0.0.0.0', port=3000,debug=True)
