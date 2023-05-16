3ac42f79a2ceee67855ba9b6d121b42c3733fcb0
curiosity <curiosity@gorengordon.com>
2023-05-16 17:30:26 +0300

    first commit

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def home():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000,debug=True)
+    app.run(host='0.0.0.0', port=3000, ssl_context=('../CuriosityMap/Keys/vrobotator_tau_ac_il_cert.cer',
+                                                    '../CuriosityMap/Keys/vrobotator_tau_ac_il_private.key'), debug=True)
