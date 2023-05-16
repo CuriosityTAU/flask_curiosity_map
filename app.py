3841cca134a5905dc48ce3a09b84b8dbafb6e1b7
PninaKlein <pninaklein9987@gmail.com>
2023-05-16 19:29:08 +0300

    first webpage - all the initial files

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/hello/')
-def hello():
-    return "Hello, World!"
-
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', ssl_context=('../CuriosityMap/vrobotator_tau_ac_il_cert.cer',
-                                         '../CuriosityMap/vrobotator_tau_ac_il_private.key'),
-            port=3000)
+    app.run(debug=True)
