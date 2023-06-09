2774d01d7ee931cb88522045bf4b02a4d6662133
curiosity@gorengordon.com <curiosity@gorengordon.com>
2023-06-10 02:32:11 +0300

    app.py file changed to fit the server and logs file added to record error logs from the flask app.

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
+import logging
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# Get the directory path of the current file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Configure the logger
+log_file_path = os.path.join(current_dir, 'logs', 'flask_error.log')
+logging.basicConfig(filename=log_file_path, level=logging.ERROR)
+
+# Enable logging for Flask
+app.logger.addHandler(logging.StreamHandler())
+app.logger.setLevel(logging.ERROR)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+# Handle uncaught exceptions
+@app.errorhandler(Exception)
+def handle_exception(e):
+    app.logger.error('Unhandled Exception: %s', e)
+    return 'Internal Server Error', 500
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
